b1dac1fca58372489df5fe0913aca69dadf0d50c
Pablorf10 <rafanet.live@gmail.com>
2021-09-04 00:18:58 -0300

    Buscando detalhes

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router";
+import axios from "axios";
 
 function ProductDetail() {
+  const { productId } = useParams();
+
+  const fecthProductDetail = async () => {
+    const { data } = await axios
+      .get(`https://fakestoreapi.com/products/${productId}`)
+      .catch((err) => console.log("Err", err));
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fecthProductDetail();
+  }, []);
+
   return (
     <div>
       <h1>Product details</h1>
