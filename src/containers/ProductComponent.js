4b97ab1e754f1f36444fd5e58f3e965f4281847e
Pablorf10 <rafanet.live@gmail.com>
2021-09-03 23:07:35 -0300

    Adicionando api de produtos e passando para action.

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function ProductComponent() {
+  const prodcts = useSelector((state) => state.allProducts.products);
+  const { id, title } = prodcts[0];
   return (
-    <div>
-      <h1>Product Component</h1>
+    <div className="four column wide">
+      <div className="ui link cards">
+        <div className="card">
+          <div className="image"></div>
+          <div className="content">
+            <div className="header">{title}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
