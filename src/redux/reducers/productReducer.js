7cae24ac20be5b02456966ac4ded91085ca0d71b
Pablorf10 <rafanet.live@gmail.com>
2021-09-03 23:29:57 -0300

    Adicionando link de redicionamento.

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,18 +1,14 @@
 import { ActionTypes } from "../constants/action-types";
 
 const initialState = {
-    products: [{
-        id: 1,
-        title: "Camisa",
-        category: "Roupa"
-    }]
-}
+  products: [],
+};
 
-export const productReducer = (state = initialState, {type, payload}) => {
-    switch (type) {
-        case ActionTypes.SET_PRODUCTS:
-            return state;
-        default:
-            return state;
-    }
-}
+export const productReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+      return { ...state, products: payload };
+    default:
+      return state;
+  }
+};
